a66ae208eb89b1639b8a93d9874e8ee73ec3e13b
GunBaseApp <phil.avery@icloud.com>
2025-05-31 13:38:48 -0500

    Add Dashboard page

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard Overview</h2>
+      <p>Welcome to the GunBase Dashboard!</p>
+    </div>
+  );
+}
+
+export default Dashboard;
